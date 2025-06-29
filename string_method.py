2889f6dc28080a8c236a69b63afd17afe332255c
Mithila <51959326+Mithila101@users.noreply.github.com>
2025-06-30 00:07:28 +0600

    Add files via upload

---
+++ string_method.py
@@ -0,0 +1,26 @@
+# Declare a string
+name = "Mithila"
+
+# Print the string
+print("Name:", name)
+
+# String concatenation
+greeting = "Hello, " + name + "!"
+print(greeting)
+
+# String length
+print("Length of name:", len(name))
+
+# Accessing characters
+print("First letter:", name[0])
+print("Last letter:", name[-1])
+
+# String slicing
+print("First 3 letters:", name[:3])
+
+# Changing case
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+
+# Checking if substring exists
+print("Does 'thil' exist in name?", "thil" in name)
